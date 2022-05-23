8ab664afe6b5db2f41b1e63da159f39fcab781d4
Marco Terrones <mterrones.ra@gmail.com>
2022-05-23 14:48:00 -0500

    dasd

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,17 @@
 import axios from 'axios'
 
-const instance = axios.create({
-  //Endpoint bakend en producción
-  baseURL: 'ec2-54-187-23-87.us-west-2.compute.amazonaws.com:9000/web-api',
+const config = {
   headers: {
     'Content-Type': 'application/json'
   }
-})
+}
+
+if (process.env.NODE_ENV === 'production') { 
+  config.baseURL = 'ec2-54-187-23-87.us-west-2.compute.amazonaws.com:9000/web-api'
+}
+
+console.log(process.env)
+
+const instance = axios.create(config)
 
 export default instance
