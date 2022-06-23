a14bab4aceda843cedf43894c3158d71cdccd2be
CristianChipana <cristianchipanahuaman@gmail.com>
2022-06-23 13:11:22 -0500

    test

--- src/pages/dashboard/dashboard.vue
+++ src/pages/dashboard/dashboard.vue
@@ -186,12 +186,10 @@ export default {
           const formData = new FormData()
 
           formData.append('file', doc.output('blob'))
-          let baseURLForApi = 'http://localhost:8000/api/send_email'
 
-          if (process.env.NODE_ENV === 'production') { 
-            // config.baseURL = process.env.VUE_APP_BACKEND_ENDPOINT
-            baseURLForApi = 'http://35.86.152.9:81/api/send_email'
-          }
+          const baseURLForApi = ( window.location.hostname.includes('localhost') )
+            ? 'http://localhost:8000/api/send_email'
+            : 'http://35.86.152.9:81/api/send_email'
 
           const response = fetch( baseURLForApi , {
             method: 'POST',
