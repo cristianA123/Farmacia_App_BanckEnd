18cf15a4a25aed0d873ffbd6941aeefc774627c1
CristianChipana <cristianchipanahuaman@gmail.com>
2022-09-30 16:06:34 -0500

    test

--- src/pages/reports/components/DetailCampaignComponent.vue
+++ src/pages/reports/components/DetailCampaignComponent.vue
@@ -13,40 +13,43 @@
         placeholder="Ej.: Filtrar por name, email, empresa, etc"
       />
     </v-card-title>
-    <v-data-table
-      :headers="headers" 
-      :items="campaigns"
-      hide-default-footer 
-    >
-      <template v-slot:[`item.actions`]="{ item }">
-        <v-menu
-          offset-y
-        >
-          <template v-slot:activator="{ attrs, on }">
-            <v-btn
-              v-bind="attrs"
-              v-on="on"
-            >
-              Acciones
-            </v-btn>
-          </template>
-          <v-list>
-            <v-list-item
-              link
-              @click="openDialogCreate(item)"
-            >
-              Modificar
-            </v-list-item>
-          </v-list>
-        </v-menu>
-      </template>
-      <p>{{ campaigns }}</p>
-    </v-data-table>
-    <v-pagination
-      v-model="pagination.current"
-      :length="pagination.total"
-      @input="ongetSms"
-    ></v-pagination>
+    <v-card-text>
+      <v-data-table
+        :headers="headers" 
+        :items="campaigns"
+        hide-default-footer 
+      >
+        <template v-slot:[`item.actions`]="{ item }">
+          <v-menu
+            offset-y
+          >
+            <template v-slot:activator="{ attrs, on }">
+              <v-btn
+                v-bind="attrs"
+                v-on="on"
+              >
+                Acciones
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item
+                link
+                @click="openDialogCreate(item)"
+              >
+                Modificar
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </template>
+        <p>{{ campaigns }}</p>
+      </v-data-table>
+      <v-pagination
+        v-model="pagination.current"
+        :length="pagination.total"
+        @input="ongetSms"
+      ></v-pagination>
+    </v-card-text>
+
   </v-card>
 </template>
 
