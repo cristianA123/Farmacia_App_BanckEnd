e97eecd225dc1fa28b636697f7446bd445789bdd
CritianChipana <cristianchipanahuaman@gmail.com>
2022-11-11 14:57:36 -0500

    NVMS9-31, DONE

--- src/pages/Urls/url.vue
+++ src/pages/Urls/url.vue
@@ -2,7 +2,7 @@
   <div class="d-flex flex-column flex-grow-1">
     <div class="d-flex align-center py-3">
       <div>
-        <div class="display-1">Acortador url</div>
+        <div class="display-1">Acortador URL</div>
       </div>
       <v-spacer></v-spacer>
       <v-spacer></v-spacer>
