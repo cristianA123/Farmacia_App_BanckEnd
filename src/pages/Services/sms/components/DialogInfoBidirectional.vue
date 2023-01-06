c66b7d3fbf1fab1e7c95ff83344280e3219b007d
Marco Terrones <mterrones.ra@gmail.com>
2023-01-06 15:47:25 -0500

    mensaje de activacion bidireccional en campanha

--- src/pages/Services/sms/components/DialogInfoBidirectional.vue
+++ src/pages/Services/sms/components/DialogInfoBidirectional.vue
@@ -6,22 +6,16 @@
         lazy-validation
         @submit.prevent="save()"
       >
-  
-        <v-card-title class="pa-2">
-          Bidireccional
-          <v-spacer></v-spacer>
-          <v-btn icon @click="dialog = false">
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
-        </v-card-title>
-  
         <v-card-text>
-          <p>Activar la función bidireccional te permite recibir respuestas de tus mensajes, estos serán contabilizados en tu cuenta.</p>
+          <v-alert type="info">
+            La función "Bidireccional" permite recibir respuestas SMS a los mensajes enviados en su campaña.
+          </v-alert>
+          <p>Cada mensaje recibido se contabilizará como 01 crédito.</p>
         </v-card-text>
   
         <v-card-actions class="pa-2">
           <v-spacer></v-spacer>
-          <v-btn color="primary" type="submit">Aceptar</v-btn>
+          <v-btn text color="primary" type="submit">Aceptar</v-btn>
         </v-card-actions>
   
       </v-form>
